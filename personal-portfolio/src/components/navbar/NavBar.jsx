c17dca5d7f18a31a670faf7f3fbd64d2f0c644a4
kundansca <kksdarmiya@gmail.com>
2024-12-08 09:56:12 +0530

    test branch

--- personal-portfolio/src/components/navbar/NavBar.jsx
+++ personal-portfolio/src/components/navbar/NavBar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function () {
   return (
     <div>
-      <p className="sm:underline">hello kundan</p>
+      <p className="sm:underline font-bold">hello kundan</p>
     </div>
   );
 }
